5f03c189f81faee09e80eb9c95c7d430289dec7a
roshni090496 <rs09041001@gmail.com>
2022-04-16 15:50:03 -0700

    first commit

---
+++ src/com/company/SmallestElement.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class SmallestElement {
+
+        public static void main(String[] args) {
+
+
+            int[] arr = new int[]{25, 11, 7, 75, 56};
+
+
+            int min = arr[0];
+
+
+            for (int j : arr) {
+
+                if (j < min)
+                    min = j;
+            }
+            System.out.println("Smallest element present in given array: " + min);
+        }
+    }
+
